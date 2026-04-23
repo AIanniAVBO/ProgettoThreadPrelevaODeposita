9dd64cf1642ca73b793f2eb8da47e8a336178826
Andrea Ianni <andrea.ianni@avbo.it>
2026-04-23 13:52:11 +0200

    #dev creato un main semplice che crea 6 operatori che fanno operazioni sui conti, 3 dal conto1 al conto2 e 3 dal conto2 al conto1

--- src/progettothreadprelevaodeposita/ProgettoThreadPrelevaODeposita.java
+++ src/progettothreadprelevaodeposita/ProgettoThreadPrelevaODeposita.java
@@ -4,6 +4,9 @@
  */
 package progettothreadprelevaodeposita;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author andre
@@ -14,7 +17,35 @@ public class ProgettoThreadPrelevaODeposita {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	// TODO code application logic here
+	//Crea due conti con 10000 come saldo iniziale
+	Conto conto1 = new Conto(10000);
+	Conto conto2 = new Conto(10000);
+	//Crea un array di operatori
+	Operatore[] ops = new Operatore[6];
+	//Crea 3 operatori che lavorano sul conto1 e fanno bonifici sul conto2
+	for (int i = 0; i < 3; ++i) {
+	    Operatore op = new Operatore(conto1, conto2);
+	    //Mermorizza l'operatore
+	    ops[i] = op;
+	    //Avvia l'operatore
+	    op.start();
+	}
+	//Crea tre operatori che lavorano sul conto2 e fanno bonifici sul conto1
+	for (int i = 0; i < 3; ++i) {
+	    Operatore op = new Operatore(conto2, conto1);
+	    //Memorizza l'operatore
+	    ops[i+3] = op;
+	    //Avvia l'operatore
+	    op.start();
+	}
+	//Aspetta che tutti terminino (in realtà non succederà mai per il while(true)
+	for (Operatore op : ops) {
+	    try {
+		op.join();
+	    } catch (InterruptedException ex) {
+		Logger.getLogger(ProgettoThreadPrelevaODeposita.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	}
     }
     
 }
